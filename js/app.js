263078334da0c183f81fc347f0001264a13af67c
Muhammad Musab <yaqoobmusab03@gmail.com>
2021-12-07 19:39:08 -0600

    add findEachMovie function and getDataForOne function

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ function handleSubmit(e) {
   e.preventDefault()
   const input = document.querySelector('input');
   if (input.value !== '') {
-    getMovies(input.value).then(data => console.log (data));
+    getMovies(input.value).then(data => findEachMovie(data.Search));
     input.value = '';
   } else {
     return;
@@ -26,5 +26,28 @@ function getMovies(movieName) {
     request.open('GET', `https://www.omdbapi.com/?s=${movieName}&apikey=dd2f7ad9`);
     request.send();
   });
-
 }
+
+function getDataForOne(movieName) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState === 4 && request.status === 200) {
+        resolve(JSON.parse(request.response));
+      } else if (request.readyState === 4) {
+        reject('Something went wrong');
+      }
+    });
+    request.open('GET', `https://www.omdbapi.com/?t=${movieName}&plot=short&apikey=dd2f7ad9`);
+    request.send();
+  });
+}
+
+
+function findEachMovie(movieArr) {
+  const titlesWrapper = document.querySelector('.titles-wrapper');
+  movieArr.forEach(movie => {
+    getDataForOne(movie.Title).then(data => console.log(data));
+  });
+}
+

f9afa55ddc18e421ad93288a6fe2a8c5762516d5
Muhammad Musab <yaqoobmusab03@gmail.com>
2021-12-07 18:32:34 -0600

    add getMovies function to get movies from search

--- js/app.js
+++ js/app.js
@@ -6,9 +6,25 @@ function handleSubmit(e) {
   e.preventDefault()
   const input = document.querySelector('input');
   if (input.value !== '') {
-    console.log(input.value);
+    getMovies(input.value).then(data => console.log (data));
     input.value = '';
   } else {
     return;
   }
+}
+
+function getMovies(movieName) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState === 4 && request.status === 200) {
+        resolve(JSON.parse(request.response));
+      } else if (request.readyState === 4) {
+        reject('Something went wrong');
+      }
+    });
+    request.open('GET', `https://www.omdbapi.com/?s=${movieName}&apikey=dd2f7ad9`);
+    request.send();
+  });
+
 }
